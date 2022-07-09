e8a5b3a9cca835d179b18c927a2a74b4564e8a30
rafikorafiko <78249003+rafikorafiko@users.noreply.github.com>
2022-07-09 16:05:44 +0700

    Update tutorial.js

--- plugins/tutorial.js
+++ plugins/tutorial.js
@@ -23,13 +23,13 @@ Penginstalannya cuy :)
 handler.help = ['tutorial']
 handler.tags = ['info']
 handler.command = /^(tutorial)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
 
-handler.admin = false
+handler.admin = true
 handler.botAdmin = false
 
 handler.fail = null
